6d01e0da5e59e94f155128048b263c00b8097f96
Leonel Zaninelli <leonelzaninellisouza@edu.unifil.br>
2025-10-26 23:13:27 -0300

    Add InvestmentNotFoundException class
    Exception

---
+++ InvestmentNotFoundException.java
@@ -0,0 +1,10 @@
+package br.com.dio.expcetion;
+
+public class InvestmentNotFoundException extends RuntimeException {
+
+    public InvestmentNotFoundException(String message) {
+        super(message);
+    }
+
+
+}
