231bb788c9050414f1a8fa790a9b522b558d0a4a
Leonel Zaninelli <leonelzaninellisouza@edu.unifil.br>
2025-11-12 15:51:59 -0300

    Delete AccountWithInvestmentException.java

--- AccountWithInvestmentException.java
+++
@@ -1,8 +0,0 @@
-package br.com.dio.expcetion;
-
-public class AccountWithInvestmentException extends RuntimeException {
-
-    public AccountWithInvestmentException(String message) {super(message);}
-
-
-}
