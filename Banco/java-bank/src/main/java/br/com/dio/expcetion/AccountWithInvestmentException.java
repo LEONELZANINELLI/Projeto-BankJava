165e99be892efe8b2da5d54f98ded242023d59d7
Leonel Zaninelli <amzaninelli@gmail.com>
2025-11-12 15:56:25 -0300

    Adicionado Projeto java-bank

---
+++ Banco/java-bank/src/main/java/br/com/dio/expcetion/AccountWithInvestmentException.java
@@ -0,0 +1,8 @@
+package br.com.dio.expcetion;
+
+public class AccountWithInvestmentException extends RuntimeException {
+
+    public AccountWithInvestmentException(String message) {super(message);}
+
+
+}
---
+++ Banco/java-bank/src/main/java/br/com/dio/expcetion/WalletNotFoundException.java
@@ -0,0 +1,8 @@
+package br.com.dio.expcetion;
+
+public class WalletNotFoundException extends RuntimeException {
+
+    public WalletNotFoundException(String message) {super(message);}
+
+    
+}
